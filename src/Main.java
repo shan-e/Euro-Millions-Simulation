220c32d7410b80f19bafdcf0db2bf2d1546c5ed0
shan-e <126803195+shan-e@users.noreply.github.com>
2025-03-28 20:05:33 +0000

    function isolated

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,18 @@ public class Main {
     public static void main(String[] args) {
         int ticketSize = 5;
         int[][] tickets = new int[ticketSize][7];
+        Main main = new Main();
+        main.generateTickets(tickets, ticketSize);
+        int[][] winningTicket = new int[1][7];
+        main.generateTickets(winningTicket, 1);
+        for (int[] x : tickets) System.out.println(Arrays.toString(x));
+        System.out.println();
+        System.out.println(Arrays.deepToString(winningTicket));
+    }
+
+    private void generateTickets(int[][] tickets, int ticketSize) {
         Random random = new Random();
         for (int i = 0; i < ticketSize; i++) for (int j = 0; j < 5; j++) tickets[i][j] = random.nextInt(1, 50);
         for (int i = 0; i < ticketSize; i++) for (int j = 5; j < 7; j++) tickets[i][j] = random.nextInt(1, 12);
-
-        for (int[] x : tickets) System.out.println(Arrays.toString(x));
     }
 }
