d287197c67c3e18ee9e7a3aebbc7296d36a3add9
shan-e <126803195+shan-e@users.noreply.github.com>
2025-03-28 19:58:16 +0000

    creating tickets

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int ticketSize = 5;
+        int[][] tickets = new int[ticketSize][7];
+        Random random = new Random();
+        for (int i = 0; i < ticketSize; i++) for (int j = 0; j < 5; j++) tickets[i][j] = random.nextInt(1, 50);
+        for (int i = 0; i < ticketSize; i++) for (int j = 5; j < 7; j++) tickets[i][j] = random.nextInt(1, 12);
+
+        for (int[] x : tickets) System.out.println(Arrays.toString(x));
     }
 }
